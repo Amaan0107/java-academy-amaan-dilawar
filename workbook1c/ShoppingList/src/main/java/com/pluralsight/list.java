e1dad8f23f479040dd3efb89df4f09b780d739dc
Amaan <adilawar2007@gmail.com>
2025-09-26 11:25:51 -0700

    cat name change

--- workbook1c/ShoppingList/src/main/java/com/pluralsight/list.java
+++ workbook1c/ShoppingList/src/main/java/com/pluralsight/list.java
@@ -2,7 +2,7 @@ package com.pluralsight;
 
 public class list {
     public static void main(String[] args) {
-        String catname = "Henny";
+        String catname = "Hennessy";
         int catage = 2;
         double catweight = 3.7;
         boolean ishungry = true;
