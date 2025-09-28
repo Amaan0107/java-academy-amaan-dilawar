9482b48cd804793b522ee5a2596250cbe155df4f
Amaan <adilawar2007@gmail.com>
2025-09-28 16:26:23 -0700

    mortgage calculator complete :)

---
+++ financial calculator/src/main/java/org/example/Main.java
@@ -0,0 +1,13 @@
+package org.example;
+import java.util.scanner;
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args)
+    Scanner scanner = new Scanner(System.in);
+    sout
+
+
+
+    }
+}
---
+++ financial calculator/src/mortgageCalculator/src/main/java/org/example/mortgage.java
@@ -0,0 +1,34 @@
+package org.example;
+import java.util.Scanner;
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class mortgage {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter Loan amount :");
+        double loan = scanner.nextDouble();
+
+        System.out.print("Enter yearly interest rate :");
+        double yearlyInterest = scanner.nextDouble();
+
+        System.out.print("Enter loan length in years :");
+        int years = scanner.nextInt();
+
+        scanner.close();
+
+        yearlyInterest = yearlyInterest/12;
+        double monthlyInterest = yearlyInterest/100;
+        int months = years * 12;
+
+        double onePlusRateToPower = Math.pow(1 + monthlyInterest, months);
+        double monthlyPayment = loan * (monthlyInterest * onePlusRateToPower) / (onePlusRateToPower - 1);
+
+        double totalPayed = monthlyPayment * months;
+        double interestRate =  totalPayed - loan;
+
+        System.out.printf("Monthly payment: $%.2f%n", monthlyPayment);
+        System.out.printf("Total interest paid: $%.2f%n", interestRate);
+
+    }
+}
