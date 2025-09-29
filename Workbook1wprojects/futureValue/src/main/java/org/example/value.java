a4656358ae26029660c8d376c432e79ef616264e
Amaan <adilawar2007@gmail.com>
2025-09-29 12:10:20 -0700

    Calc projects done

--- Workbook1wprojects/futureValue/src/main/java/org/example/value.java
+++ Workbook1wprojects/futureValue/src/main/java/org/example/value.java
@@ -2,14 +2,14 @@ package org.example;
 import java.util.Scanner;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class valueCalc {
+public class value {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         System.out.printf("Enter deposit amount: ");
         double depositAmount = scanner.nextDouble();
 
-        System.out.printf("Enter interest rate (1.80): ");
+        System.out.printf("Enter interest rate (1.90): ");
         double interestRate = scanner.nextDouble();
 
         System.out.printf("Total years for CD to mature: ");
@@ -17,7 +17,7 @@ public class valueCalc {
 
         interestRate = interestRate/100;
 
-        double futureValue =depositAmount * Math.pow(1 + interestRate/365, 365 * years);
+        double futureValue = depositAmount * Math.pow(1 + interestRate/365, 365 * years);
 
         double interest = futureValue - depositAmount;
 
