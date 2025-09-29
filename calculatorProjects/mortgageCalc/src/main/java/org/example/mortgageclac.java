8c526598d2764b55666b347f84401f2a5b99347b
Amaan <adilawar2007@gmail.com>
2025-09-29 11:50:50 -0700

    workbook1w done

--- calculatorProjects/mortgageCalc/src/main/java/org/example/mortgageclac.java
+++ calculatorProjects/mortgageCalc/src/main/java/org/example/mortgageclac.java
@@ -2,7 +2,7 @@ package org.example;
 import java.util.Scanner;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class mortgage {
+class mortgageCalc {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
---
+++ calculatorProjects/valueCalc/src/main/java/org/example/valueCalc.java
@@ -0,0 +1,27 @@
+package org.example;
+import java.util.Scanner;
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class valueCalc {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.printf("Enter deposit amount: ");
+        double depositAmount = scanner.nextDouble();
+
+        System.out.printf("Enter interest rate (1.90): ");
+        double interestRate = scanner.nextDouble();
+
+        System.out.printf("Total years for CD to mature: ");
+        int years = scanner.nextInt();
+
+        interestRate = interestRate/100;
+
+        double futureValue =depositAmount * Math.pow(1 + interestRate/365, 365 * years);
+
+        double interest = futureValue - depositAmount;
+
+        System.out.printf("Future value is: $%.2f\n", futureValue);
+        System.out.printf("Total interest earned: $%.2f\n", interest);
+    }
+}
