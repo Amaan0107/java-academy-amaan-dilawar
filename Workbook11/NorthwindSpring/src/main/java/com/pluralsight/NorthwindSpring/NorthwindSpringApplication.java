81f3869ff28bb0678a83add1eb0ee57bc7d4bb5e
Amaan0107 <Adilawar2007@gmail.com>
2025-12-10 09:38:35 -0800

    Lets see if it commits

---
+++ Workbook11/NorthwindSpring/src/main/java/com/pluralsight/NorthwindSpring/NorthwindSpringApplication.java
@@ -0,0 +1,13 @@
+package com.pluralsight.NorthwindSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NorthwindSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NorthwindSpringApplication.class, args);
+	}
+
+}
---
+++ Workbook11/NorthwindSpring/src/main/java/com/pluralsight/NorthwindSpring/Product.java
@@ -0,0 +1,26 @@
+package com.pluralsight.NorthwindSpring;
+
+public class Product {
+    private int productId;
+    private String name;
+    private String category;
+    private double price;
+
+    public Product(int productId, String name, String category, double price) {
+        this.productId = productId;
+        this.name = name;
+        this.category = category;
+    }
+    public int getProductId() {
+        return productId;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getCategory() {
+        return category;
+    }
+    public double getPrice() {
+        return price;
+    }
+}
